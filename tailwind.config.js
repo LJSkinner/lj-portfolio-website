cc51008887c638ee730d90aec2641a7ddf1af5a5
Luke S <hyperconix@pm.me>
2022-12-14 01:06:20 +0000

    feat(tailwind): add colours and breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px"
+    },
+    extend: {
+      colors: {
+        darkNavyGrey: "#404258",
+        lightGrey: "#6B728E",
+        lightNavyGrey: "#474E68",
+        navyGreyPurple: "#50577A"
+      }
+    },
   },
   plugins: [],
 }
